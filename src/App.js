03ba4c6dc73e91b5a81658c4b0976337e51799a0
Pedro Gutierrez Jr <gutierrezpedro2112@gmail.com>
2023-08-16 12:34:07 -0500

    fixed module conflict with home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import Nav from "./components/Navbar/Nav";
 import Projects from "./pages/Projects";
 import Footer from "./components/Footer/Footer";
